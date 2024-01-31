309509332486e03e9d94dcde5fe9961f13b68483
SavioHaistem <savio.dantas.5858@gmail.com>
2024-01-31 16:59:39 -0300

    (feat): create project

---
+++ src/main/java/com/coffebuilder/CoffeBuilder/CoffeBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.coffebuilder.CoffeBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffebuilder/CoffeBuilder/CoffeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffebuilder.CoffeBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
